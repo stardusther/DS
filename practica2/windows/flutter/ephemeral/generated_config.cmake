405b8de0ce3a33bfc7db19ea17dbf2cce4c7116b
Esther García <esther.g.g.01@gmail.com>
2022-05-23 22:56:27 +0200

    initial commit

---
+++ practica2/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Documentos\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Documentos\\GitHub\\DS\\practica2" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Documentos\\flutter"
+  "PROJECT_DIR=D:\\Documentos\\GitHub\\DS\\practica2"
+  "FLUTTER_ROOT=D:\\Documentos\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Documentos\\GitHub\\DS\\practica2\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Documentos\\GitHub\\DS\\practica2"
+  "FLUTTER_TARGET=D:\\Documentos\\GitHub\\DS\\practica2\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Documentos\\GitHub\\DS\\practica2\\.dart_tool\\package_config.json"
+)
